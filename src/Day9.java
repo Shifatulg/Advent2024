4bfcc371d55d23ab2917c2bd2390eea29f860987
Shifat <sample@bths.edu>
2024-12-12 13:20:07 -0500

    WIP Day 9 Part 1

--- src/Day9.java
+++ src/Day9.java
@@ -6,13 +6,16 @@ public class Day9 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         String fileName = "src/Day9.txt";
-        String code = "";
+        ArrayList<String> codes = new ArrayList<>();
+
         try {
             File f = new File(fileName);
             Scanner s = new Scanner(f);
             while (s.hasNextLine()) {
+                String code = "";
                 String line = s.nextLine();
                 code = line;
+                codes.add(code);
                 if (!line.equals("")) {
 
                 }
@@ -22,18 +25,36 @@ public class Day9 {
         boolean buffer = true;
         int num = 0;
         String decodedString = "";
-        for (int i = 0; i < code.length(); i++) {
-            int input = Integer.parseInt(code.substring(i,i+1));
-            buffer = !buffer;
-            if (buffer) {
-                for (int x = 0; x < input; x++) {
-                    decodedString += ".";
+        for (String j : codes ) {
+            for (int i = 0; i < j.length(); i++) {
+                int input = Integer.parseInt(j.substring(i, i + 1));
+                buffer = !buffer;
+                if (buffer) {
+                    for (int x = 0; x < input; x++) {
+                        decodedString += ".";
+                    }
+                } else {
+                    for (int x = 0; x < input; x++) {
+                        decodedString += num;
+                    }
+                    num++;
+                    if (num == 10) {
+                        num = 0;
+                    }
                 }
-            } else {
-                for (int x = 0; x < input; x++) {
-                    decodedString += num;
+            }
+        }
+
+        for (int i = 0; i < decodedString.length(); i++) {
+            if (decodedString.substring(i,i+1).equals(".")) {
+                for (int j = decodedString.length(); j > 0; j--) {
+                    if (!decodedString.substring(j-1,j).equals(".")) {
+                        String fragmentOne = decodedString.substring(0,i-1);
+                        String fragmentTwo = decodedString.substring(i+1,j-1);
+                        String fragmentThree = decodedString.substring(j) + ".";
+                        decodedString = fragmentThree + fragmentTwo + fragmentOne;
+                    }
                 }
-                num++;
             }
         }
         System.out.println(decodedString);
